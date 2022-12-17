df9fc4b28b19e5c223838fbb6db768a8fbe63fa0
Anna <kovalenkoani2508@gmail.com>
2022-12-17 22:48:11 +0300

    задач41

---
+++ задача41/Program.cs
@@ -0,0 +1,43 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+
+int m = new Random().Next(1,10);
+Console.WriteLine ($"Введите {m} положительных и отрицательных чисел");
+int [] numbers = new int [m];
+
+Console.WriteLine(creatArray(numbers));
+Console.WriteLine($"В массиве {minusSum(numbers)} отрицательных чисел");
+
+int [] creatArray (int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine ($"Введите число: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+int minusSum(int [] array)
+{
+    int count = 0;
+    foreach(int a in array )
+    {
+        if (a<0) count++;
+    }
+    return count;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
